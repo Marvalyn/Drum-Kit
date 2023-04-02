05bdc2b3edf41500765dacec107db31b7403f24c
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-04-02 15:13:20 +0100

    added if statement to stop function from running if there is no audio element associated with the selected key

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown', function(e){
 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 console.log(audio);
+
+//if there is no audio element, stop the function from running
+if(!audio)
+return;
 });
