364f44d7a0338dd164daa015dad7e7549a431f35
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-04-02 15:25:19 +0100

    added key const to select key class

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 window.addEventListener('keydown', function(e){
+    //selecting audio elements using the data-key attribute
 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
 console.log(audio);
 
+// selecting element with class of key and the element with data-key attribute
+const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+console.log(key)
 //if there is no audio element, stop the function from running
 if(!audio)
 return;
